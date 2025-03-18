fd1e3859b26c79d01a7c7d31006b2de1a9f9c98f
A.HAMED <133318802+AHMEDMOM1@users.noreply.github.com>
2025-03-18 22:14:17 +0300

    Update Gemstones.cpp

--- Problem-Solving/level 1/HackerRank/Gemstones.cpp
+++ Problem-Solving/level 1/HackerRank/Gemstones.cpp
@@ -1,53 +1,4 @@
-/*
-Problem Statement
-
-There is a collection of rocks where each rock has various minerals embedded in it. Each type of mineral is designated by a lowercase letter in the range ascii[a-z].
-There may be multiple occurrences of a mineral in a rock. A mineral is called a gemstone if it occurs at least once in each of the rocks in the collection.
-Given a list of minerals embedded in each of the rocks, display the number of types of gemstones in the collection.
-
-Example
-
-plaintext
-arr = ['abc', 'abc', 'bc']
-The minerals `b` and `c` appear in each rock, so there are 2 gemstones.
-Function Description
-
-Complete the gemstones function in the editor below.
-
-gemstones** has the following parameter(s):
- string arr[n]: an array of strings
-
-Returns 
- int: the number of gemstones found
-
-Input Format
-
-The first line consists of an integer n, the size of arr.
-Each of the next n lines contains a string arr[i] where each letter represents an occurrence of a mineral in the current rock.
-
-Constraints
-- 1 ≤ n ≤ 100
-- 1 ≤ | arr[i] | ≤ 100
-- Each composition arr[i] consists of only lower-case Latin letters ('a'-'z').
-
-Sample Input
-plaintext
-
-STDIN        Function
------        --------
-3            arr[] size n = 3
-abcdde       arr = ['abcdde', 'baccd', 'eeabg']
-baccd
-eeabg
-
-Sample Output
-plaintext
-2
-
-Explanation
-Only `a` and `b` occur in every rock.
------------------------------------------------------
-*/
+//Question link : https://www.hackerrank.com/challenges/gem-stones/problem
 
 #include <iostream>
 #include <string>   
@@ -121,4 +72,4 @@ int main() {
 	cout << calcGemStone(arr, uniqueLetter, sizeOfMatrix);
 
 	return 0;
-}//Question link : https://www.hackerrank.com/challenges/gem-stones/problem
+}
