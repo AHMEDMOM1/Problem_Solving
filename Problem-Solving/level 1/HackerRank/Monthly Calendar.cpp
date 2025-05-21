e098e6f723b150f06481c26c96e57dd4f08ea736
A.HAMED <133318802+AHMEDMOM1@users.noreply.github.com>
2025-05-21 23:15:49 +0300

    Create Monthly Calendar.cpp

---
+++ Problem-Solving/level 1/HackerRank/Monthly Calendar.cpp
@@ -0,0 +1,12 @@
+// Write a program that prints the monthly calendar like this:
+/*
+______________Marc_______________
+sun  Mon  Tue  Wed  Thu  Fri  Sat
+                              1
+2    3    4    5    6    7    8
+9    10   11   12   13   14   15
+16   17   18   19   20   21   22
+23   24   25   26   27   28   29
+30   31
+_________________________________
+*/
