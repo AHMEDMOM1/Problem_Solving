bd3dd956bbb9fc5af10983b794bb404c0417f340
A.HAMED <133318802+AHMEDMOM1@users.noreply.github.com>
2025-05-22 10:52:44 +0300

    Update Monthly Calendar.cpp

--- Problem-Solving/level 1/HackerRank/Monthly Calendar.cpp
+++ Problem-Solving/level 1/HackerRank/Monthly Calendar.cpp
@@ -14,15 +14,21 @@ _________________________________
 //######################################
 #include <iostream>
 #include <iomanip>
+#include <cmath>
+
 using namespace std;
 
-struct Date {
-    int year{};
-    int month{};
-    int day{1};
+struct Data {
+	short Day{1};
+	short Month{};
+	short Year{};
 };
 
-short getNumberOfDaysFromMonth(int month, int year) {
+bool checkIfLeapYear(int year) {
+	return year % 400 == 0 || year % 4 == 0 && year % 100 != 0;
+}
+
+short getNumberOfDaysInMonth(short month, short year) {
 	if (month == 2) return checkIfLeapYear(year) ? 29 : 28;
 	else if (month < 7)
 		return (month % 2 == 0) ? 30 : 31;
@@ -30,77 +36,74 @@ short getNumberOfDaysFromMonth(int month, int year) {
 		return (month % 2 == 0) ? 31 : 30;
 }
 
-short getDayOfWeek(const Date date) {
-    int a = (14 - date.month) / 12;
-    int y = date.year - a;
-    int m = date.month + (12 * a) - 2;
-    short dayOfWeek = (date.day + y + (y / 4) - (y / 100) + (y / 400) + ((31 * m) / 12)) % 7;
-    return dayOfWeek;
+Data fillDataInfo() {
+	Data data{};
+	cout << "Year: ";
+	cin >> data.Year;
+
+	cout << "Month: ";
+	cin >> data.Month;
+
+	return data;
 }
 
-bool isLeapYear(int year) {
-    return year % 400 == 0 || year % 4 == 0 && year % 100 != 0;
+short getDayNum(Data data) {
+	int a = (14 - data.Month) / 12;
+	int y = data.Year - a;
+	int m = data.Month + (12 * a) - 2;
+	short d = (data.Day + y + (y / 4) - (y / 100) + (y / 400) + ((31 * m) / 12)) % 7;
+	return d;
 }
 
-Date inputBirthday() {
-    Date date{};
-    cout << "Year: ";
-    cin >> date.year;
-
-    cout << "Month: ";
-    cin >> date.month;
-
-    return date;
+string getMonthName(short monthNumber) {
+	string Month[]{
+		"","jan", "Feb", "Marc", "Apr",
+		"May", "Jun", "Jul", "Aug",
+		"Sep", "Cot", "Nov", "Dec"
+	};
+	return Month[monthNumber];
 }
 
-void printCalendarHeader(int month) {
-    string months[]{
-        "", "Jan", "Feb", "Mar", "Apr",
-        "May", "Jun", "Jul", "Aug",
-        "Sep", "Oct", "Nov", "Dec"
-    };
-    string days[]{ "", "Sun", "Mon", "Tue", "Wed",
-            "Thu", "Fri", "Sat" };
-
-    cout << setw(18) << setfill('_') << months[month]
-         << setw(15) << '_' << setfill(' ') << endl;
-    for (int i{1}; i < 8; i++) {
-        cout << setw(5) << left << days[i];
-    } cout << endl;
+void showHeaderCalender(short monthNum) {
+	string Days[]{ "", "Sun", "Mon", "Tue", "Wed",
+			"Thu", "Fri", "Sat" };
+	cout << setw(18) << setfill('_') << right << getMonthName(monthNum)
+	     << setw(15) << '_' << setfill(' ') << endl;
+	for (short i{ 1 }; i < 8; i++) {
+		cout << setw(5) << left << Days[i];
+	}cout << endl;
 }
 
-void printCalendarDays(const Date date) {
-    short numDaysInMonth = getDaysInMonth(date.month, date.year);
-    short firstDayOfWeek = getDayOfWeek(date);
-    short currentDay{1};
-    for (short i{}; i < 7; i++) {
-        if (i >= firstDayOfWeek) {
-            cout << currentDay++;
-        }
-        else cout << ' ';
-        cout << setw(4) << left << ' ';
-    } cout << endl;
-    // floor for return the last number
-    // (numberOfMunth + numOfday) for space in first tabel + number of days in month
-    // *. 142 = /7 is column
-    // floor((numOfMunth + numOfday)*.142) for compiler know how much row we need
-    for (short i{}; i < floor((numDaysInMonth + firstDayOfWeek)*.142); i++) {
-        for (short j{}; j < 7; j++) {
-            if (currentDay <= numDaysInMonth) {
-                cout << setw(5) << left << currentDay++;
-            }
-        } cout << endl;
-    }
+void showBodyCalender(short dayNum, Data data) {
+	short monthNum = getNumberOfDaysInMonth(data.Month, data.Year);
+	short counter{ 1 };
+	for (short i{ 0 }; i < 7; i++) { // i && monthnum started from 0
+		if (i >= dayNum)
+			cout << setw(5) << left << counter++;
+		else
+			cout << setw(5) << ' ';
+	}cout << endl;
+	short row = ceil((monthNum - counter) * .142);
+	short column{ 7 };
+	for (short i{}; i < row; i++) {
+		for (short j{}; j < column; j++) {
+			if (counter <= monthNum)
+				cout << setw(5) << left << counter++;
+			else
+				cout << setw(5) << ' ';
+		}cout << endl;
+	}
 }
 
-void displayCalendar() {
-    Date date{inputBirthday()};
-    printCalendarHeader(date.month);
-    printCalendarDays(date);
-    cout << setw(33) << setfill('_') << '_';
+void showCalenderList() {
+	Data data{ fillDataInfo() };
+	short dayNum = getDayNum(data);
+	showHeaderCalender(data.Month);
+	showBodyCalender(dayNum, data);
+	cout << setw(33) << setfill('_') << '_' << setfill(' ') << endl;
 }
 
 int main() {
-    displayCalendar();
-    return 0;
+	showCalenderList();
+	return 0;
 }
